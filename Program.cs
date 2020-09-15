522e102b070a96a7ed38f0cd418eb9a038a1ae59
Wei Ming Guo <weimingguo@usf.edu>
2020-09-15 15:46:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tech_Project_2B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user to input their expected grade for ISM 4300
+            Console.WriteLine("Please enter an interger value of the grade you expect to get in ISM4300");
+
+            try
+            {
+                // This variable will gather the user input
+                string input = Console.ReadLine();
+
+            }
+        }
+    }
+}
